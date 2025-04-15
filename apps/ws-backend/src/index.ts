8bb9304979d4f57bd7c9037b602a8a1533178d94
shivamprajapatii <shivamprajapati4794@gmail.com>
2025-04-15 19:44:37 +0530

    working on ws-backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,6 +4,20 @@ import { JWT_SECRET } from '@repo/backend-common/config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function checkUser(token : string) : string | null {
+  const decode = jwt.verify(token, JWT_SECRET);
+
+  if(typeof decode === "string"){
+    return null;
+  }
+
+  if(!decode || !decode.userId){
+    return null;
+  }
+
+  return decode.userId;
+}
+
 wss.on('connection', function connection(ws, request) {
   ws.on('error', console.error);
 
@@ -13,20 +27,13 @@ wss.on('connection', function connection(ws, request) {
   }
 
   const queryParams = new URLSearchParams(url.split('?')[1]);
-
   const token = queryParams.get('token') || "";
-  const decode = jwt.verify(token, JWT_SECRET);
+  const userId = checkUser(token);
 
-  if(typeof decode === "string"){
+  if(!userId){
     ws.close();
-    return;
   }
-
-  if(!decode || !decode.userId){
-    ws.close();
-    return ;
-  }
-
+ 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
